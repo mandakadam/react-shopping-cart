82776806179cc67349b4d86e7067eebf1df9ae62
Manda <mandakadam@gmail.com>
2020-03-09 00:28:28 +0530

    Updates

---
+++ precache-manifest.11aa1652a2a0c70119511e334c04bfbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c0395507f2d2ab0219898b9142f804",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "da2439eeacd58e49b9bb",
+    "url": "/react-shopping-cart/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "052ccb1f5d27e8ff89c7",
+    "url": "/react-shopping-cart/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "da2439eeacd58e49b9bb",
+    "url": "/react-shopping-cart/static/js/2.d9797178.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-shopping-cart/static/js/2.d9797178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052ccb1f5d27e8ff89c7",
+    "url": "/react-shopping-cart/static/js/main.ca90ea57.chunk.js"
+  },
+  {
+    "revision": "8c3e24d15e5f12b1e9d7",
+    "url": "/react-shopping-cart/static/js/runtime-main.c8904998.js"
+  }
+]);
